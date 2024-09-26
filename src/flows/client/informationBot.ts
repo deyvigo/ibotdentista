dca4f986c79f59fcbd02fc3096e255a86e55f119
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-09-26 15:09:56 -0500

    fix: message information bot

--- src/flows/client/informationBot.ts
+++ src/flows/client/informationBot.ts
@@ -4,12 +4,13 @@ import { sendText } from '../../services/bot/sendText'
 export const informationClientBot = async (socket: WASocket, messageInfo: proto.IWebMessageInfo) => {
   const from = messageInfo.key.remoteJid as string
 
-  await sendText(socket, from!, 'Hola, soy Leopoldo, un asistente chatbot de la Clínica Dental Tapia y Asociados. Puedes consultar las siguientes acciones:')
-  await sendText(socket, from!, '- Puedes consultar el horario de trabajo del doctor.')
-  await sendText(socket, from!, '- Puedes ver las citas que tienes pendientes.')
-  await sendText(socket, from!, '- Puedes agendar una nueva cita (solo una por número de teléfono).')
-  await sendText(socket, from!, '- Puedes cancelar citas.')
-  await sendText(socket, from!, '- Puedes consultar los servicios que ofrece el consultorio dental.')
-  await sendText(socket, from!, '- Puedes preguntar sobre tratamientos o pequeñas consultas dentales.')
-  await sendText(socket, from!, '- Puedes preguntar por la dirección de la clínica dental.')
+  await sendText(socket, from!, 'Las acciones con las que te puedo ayudar son:')
+  
+  await sendText(socket, from!, 'Consultar el horario de trabajo del doctor.')
+  await sendText(socket, from!, 'Ver las citas que tienes pendientes.')
+  await sendText(socket, from!, 'Agendar una nueva cita (solo una por número de teléfono).')
+  await sendText(socket, from!, 'Cancelar citas.')
+  await sendText(socket, from!, 'Consultar los servicios que ofrece el consultorio dental.')
+  await sendText(socket, from!, 'Preguntar sobre tratamientos o pequeñas consultas dentales.')
+  await sendText(socket, from!, 'Preguntar por la dirección de la clínica dental.')
 }
