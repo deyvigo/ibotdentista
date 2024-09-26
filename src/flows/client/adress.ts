e1077edf4f4453d1e70718f2af080c66b86c8769
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-09-26 15:09:28 -0500

    fix: adres refactor

--- src/flows/client/adress.ts
+++ src/flows/client/adress.ts
@@ -4,6 +4,6 @@ import { sendText } from '../../services/bot/sendText'
 export const adress = async (socket: WASocket, messageInfo: proto.IWebMessageInfo) => {
   const from = messageInfo.key.remoteJid as string
 
-  await sendText(socket, from!, 'Claro, esta es la dirección de la clínica: ')
+  await sendText(socket, from!, 'Esta es la dirección de la clínica: ')
   await sendText(socket, from!, 'https://maps.app.goo.gl/Z8YNugE5Hc8cHnUV7')
 }
